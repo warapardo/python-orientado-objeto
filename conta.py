11e9eff1261c893b1027b1684823a6628b59adba
wara <waarapardo@gmail.com>
2024-04-11 20:36:21 -0300

    Finalização do curso Python Orientação a Objetos

--- conta.py
+++ conta.py
@@ -4,16 +4,52 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero #atributo privado quando se tem o "__" depois do self
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        self.__codigo_banco
 
-    def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+    def extrato(self): #metódo
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor): #metódo privado quando se tem o "__" antes do nome do metódo
+        valor_disponivel = self.__saldo + self.__limite
+        return valor <= valor_disponivel
 
     def saca(self, valor):
-        self.saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self): #getter
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, valor): #setter
+        self.__limite = valor
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
